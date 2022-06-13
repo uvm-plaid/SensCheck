12511b18283fb64a962080332087c03ece1fdf88
psilospore <syedjafri99@gmail.com>
2022-06-12 23:00:25 -0500

    feat/refactor: Successfully running addition and solo add. Needed to generate matrixes of the same size. Move quickcheck stuff to VerifierQuickCHeck.

--- src/Verifier.hs
+++ src/Verifier.hs
@@ -1,11 +1,6 @@
 {- HLINT ignore "Use camelCase" -}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Verifier where
@@ -13,10 +8,7 @@ module Verifier where
 import Control.Monad (replicateM)
 import qualified Data.Matrix as Matrix
 import Debug.Trace (trace)
-import Primitives
-import Sensitivity
-import Sensitivity (SDouble)
-import Test.QuickCheck (Arbitrary (arbitrary), quickCheck)
+import Sensitivity (NMetric (Diff), SDouble (..), type (+++))
 
 -- This is a function from an external library for example hmatrix
 unsafe_f :: Double -> Double -> Double
@@ -34,31 +26,12 @@ prop_distance x1 y1 x2 y2 =
 f :: forall s1 s2. SDouble Diff s1 -> SDouble Diff s2 -> SDouble Diff (s1 +++ s2)
 f a b = D_UNSAFE $ unSDouble a + unSDouble b
 
-instance Arbitrary (SDouble Diff '[]) where
-  arbitrary = D_UNSAFE <$> arbitrary @Double
-
--- Required for quickCheck
-instance Show (SDouble Diff '[]) where
-  show sdouble = "SDouble Diff '[] " <> show (unSDouble sdouble)
-
--- TODO at a later point create Arbitrary instance for: forall s. SDouble Diff s
-
 prop_distance_solo :: SDouble Diff '[] -> SDouble Diff '[] -> SDouble Diff '[] -> SDouble Diff '[] -> Bool
 prop_distance_solo a1 a2 b1 b2 =
   let d1 = abs $ unSDouble a1 - unSDouble a2
       d2 = abs $ unSDouble b1 - unSDouble b2
       dout = abs $ unSDouble (f a1 b1) - unSDouble (f a2 b2)
    in dout <= d1 + d2 + 0.000000001
-
-instance Arbitrary a => Arbitrary (Matrix.Matrix a) where
-  arbitrary = do
-    -- Generate an arbitrary number of rows and columns
-    row <- arbitrary @Int
-    col <- arbitrary @Int
-    -- Generate a list of arbitrary elements of size row * col
-    elems <- replicateM (row * col) arbitrary
-    -- Convert to Matrix
-    pure $ Matrix.fromList row col elems
 
 -- Without Solo - L2 sensitivity of matrix addition
 safe_add :: Matrix.Matrix Double -> Matrix.Matrix Double -> Matrix.Matrix Double
@@ -72,15 +45,29 @@ prop_safe_add ::
   Bool
 prop_safe_add a1 a2 b1 b2 =
   let d1 = norm_2 (a1 - a2) -- L2 distance between first arguments
-      d2 = norm_2 (b1 - b2) -- L2 distance between second arguments
+      d2 = norm_2 (b1 - b2) -- LsuchThat2 distance between second arguments
       -- L2 distance between two outputs
       dout = norm_2 $ (safe_add a1 b1) - (safe_add a2 b2)
    in dout <= d1 + d2 + 0.000000001
+
+-- L2 norm of a Matrix
+norm_2 :: Floating n => Matrix.Matrix n -> n
+norm_2 m = sqrt $ foldr (\x acc -> acc + abs x ** 2) 0 m
 
 -- With Solo - L2 sensitivity of matrix addition
 safe_add_solo :: Matrix.Matrix (SDouble Diff s1) -> Matrix.Matrix (SDouble Diff s2) -> Matrix.Matrix (SDouble Diff (s1 +++ s2))
 safe_add_solo m1 m2 = D_UNSAFE <$> (unSDouble <$> m1) + (unSDouble <$> m2)
 
--- L2 norm of a Matrix
-norm_2 :: Floating n => Matrix.Matrix n -> n
-norm_2 m = sqrt $ foldr (\x acc -> acc + abs x ** 2) 0 m
+prop_safe_add_solo ::
+  Matrix.Matrix (SDouble 'Diff '[]) ->
+  Matrix.Matrix (SDouble 'Diff '[]) ->
+  Matrix.Matrix (SDouble 'Diff '[]) ->
+  Matrix.Matrix (SDouble 'Diff '[]) ->
+  Bool
+prop_safe_add_solo a1 a2 b1 b2 =
+  let toDoubleMatrix m = unSDouble <$> m
+      d1 = norm_2 $ toDoubleMatrix a1 - toDoubleMatrix a2 -- L2 distance between first arguments
+      d2 = norm_2 $ toDoubleMatrix b1 - toDoubleMatrix b2
+      -- L2 distance between two outputs
+      dout = norm_2 $ toDoubleMatrix (safe_add_solo a1 b1) - toDoubleMatrix (safe_add_solo a2 b2)
+   in dout <= d1 + d2 + 0.000000001
