79346cb81dcd88a15d0c103970dd1893102fd999
Syed Jafri <syedjafri99@gmail.com>
2022-07-31 23:34:04 -0400

    feat: Checkpoint where I attempted using SEnv' -> Term'. Going to try as a type argument instead

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,11 @@ module Main where
 
 import Language.Haskell.TH
 import TH
+import TestFunctions (allTypes)
 import Verifier
 
 -- $((: []) <<= genProp 'safe_add)
 
 main :: IO ()
 main = do
-  putStrLn $(stringE . pprint =<< genProp 'f)
+  putStrLn $(stringE . pprint =<< genProp 'allTypes)
