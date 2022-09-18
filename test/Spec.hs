838598e600180e486f07fceb27319bcbadfa8b4f
psilospore <syedjafri99@gmail.com>
2022-09-17 20:02:04 -0400

    feat: Create arbitary instance for SMatrix and fail

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,8 +3,9 @@ import Test.QuickCheck (quickCheck, withMaxSuccess)
 import AnnotatedExternalLibrary (external_function, add_matrix_solo)
 import TH (genMainQuickCheck)
 import DistanceFunctions
+import Utils
 
-$(genMainQuickCheck "tests" ['external_function])
+$(genMainQuickCheck "tests" ['external_function, 'add_matrix_solo])
 
 main :: IO ()
 main = tests
