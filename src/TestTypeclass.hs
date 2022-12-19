873a3cdea3f0c86da8541b24b619b5c48eb44c35
psilospore <syedjafri99@gmail.com>
2022-12-18 23:06:24 -0500

    temp

--- src/TestTypeclass.hs
+++ src/TestTypeclass.hs
@@ -12,3 +12,16 @@ instance A [i] where
 instance A Integer where
   tcmember a = "I am a integer"
 
+-- TODO try Joe's thing without a proxy and see if it works
+-- Question how does this approach know what a is
+-- Well I think it actually might when you invoke the function
+-- I'm guessing the error might not be too useful
+
+class Distance a where
+  distance :: a -> a -> Double
+
+instance Distance [i] where
+  distance a b = 3
+
+instance Distance Integer where
+  distance a b = 3
