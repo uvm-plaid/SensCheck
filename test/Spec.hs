35b0e328f448810877564bba966511c64e339e3e
Syed Jafri <syedjafri99@gmail.com>
2022-12-26 18:35:37 -0500

    feat: Added a case that is implemented incorrectly

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,14 +2,14 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -ddump-splices #-}
 
-import AnnotatedExternalLibrary (add_matrix_solo, solo_plus)
+import AnnotatedExternalLibrary (add_matrix_solo, solo_plus, solo_plus_incorrect)
 import DistanceFunctions
 import Sensitivity
 import TH (genMainQuickCheck)
 import Test.QuickCheck (quickCheck, withMaxSuccess)
 import Utils
 
-$(genMainQuickCheck "tests" ['solo_plus, 'add_matrix_solo])
+$(genMainQuickCheck "tests" ['solo_plus, 'add_matrix_solo, 'solo_plus_incorrect])
 
 main :: IO ()
 main = tests
