435cc756fd4e30836854bc88919a4284d7872f45
Syed Jafri <syedjafri99@gmail.com>
2023-07-26 00:06:43 -0400

    Attempting a less polymorphic solution

--- src/DpMinst.hs
+++ src/DpMinst.hs
@@ -17,26 +17,22 @@ import Control.Applicative
 import Control.Monad
 import Control.Monad.Random
 import Control.Monad.Trans.Except
-
 import Data.Attoparsec.Text qualified as A
 import Data.List (foldl')
 #if ! MIN_VERSION_base(4,13,0)
 import           Data.Semigroup ( (<>) )
 #endif
+import Data.Singletons.Decide
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Data.Vector.Storable qualified as V
-
-import Numeric.LinearAlgebra (maxIndex)
-import Numeric.LinearAlgebra.Static qualified as SA
-
-import Options.Applicative
-
+import GHC.Generics ((:+:) (L1))
+import GHC.TypeLits qualified as TL
 import Grenade
 import Grenade.Utils.OneHot
-
-import Data.Singletons.Decide
-import GHC.TypeLits qualified as TL
+import Numeric.LinearAlgebra (maxIndex)
+import Numeric.LinearAlgebra.Static qualified as SA
+import Options.Applicative
 import Prelude.Singletons (Head, Last, SingI)
 import Primitives qualified as Solo
 import Privacy qualified as Solo
@@ -64,33 +60,42 @@ type FL i o =
 -- /NOTE:/ This model is actually too complex for MNIST, and one should use the type given in the readme instead.
 --         This one is just here to demonstrate Inception layers in use.
 --
+
 type MNIST =
-  Network
-    '[ Reshape
-     , Concat ('D3 28 28 1) Trivial ('D3 28 28 14) (InceptionMini 28 28 1 5 9)
-     , Pooling 2 2 2 2
-     , Relu
-     , Concat ('D3 14 14 3) (Convolution 15 3 1 1 1 1) ('D3 14 14 15) (InceptionMini 14 14 15 5 10)
-     , Crop 1 1 1 1
-     , Pooling 3 3 3 3
-     , Relu
-     , Reshape
-     , FL 288 80
-     , FL 80 10
-     ]
-    '[ 'D2 28 28
-     , 'D3 28 28 1
-     , 'D3 28 28 15
-     , 'D3 14 14 15
-     , 'D3 14 14 15
-     , 'D3 14 14 18
-     , 'D3 12 12 18
-     , 'D3 4 4 18
-     , 'D3 4 4 18
-     , 'D1 288
-     , 'D1 80
-     , 'D1 10
-     ]
+  Network Layers Shapes'
+
+-- Layers in network
+type Layers =
+  '[ Reshape,
+     Concat ('D3 28 28 1) Trivial ('D3 28 28 14) (InceptionMini 28 28 1 5 9),
+     Pooling 2 2 2 2,
+     Relu,
+     Concat ('D3 14 14 3) (Convolution 15 3 1 1 1 1) ('D3 14 14 15) (InceptionMini 14 14 15 5 10),
+     Crop 1 1 1 1,
+     Pooling 3 3 3 3,
+     Relu,
+     Reshape,
+     FL 288 80,
+     FL 80 10
+   ]
+
+-- The shape
+type Shapes' =
+  '[ 'D2 28 28,
+     'D3 28 28 1,
+     'D3 28 28 15,
+     'D3 14 14 15,
+     'D3 14 14 15,
+     'D3 14 14 18,
+     'D3 12 12 18,
+     'D3 4 4 18,
+     'D3 4 4 18,
+     'D1 288,
+     'D1 80,
+     'D1 10
+   ]
+
+type LastShape = Last Shapes'
 
 randomMnist :: MonadRandom m => m MNIST
 randomMnist = randomNetwork
@@ -101,18 +106,18 @@ convTest iterations trainFile validateFile rate = do
   trainData <- readMNIST trainFile
   validateData <- readMNIST validateFile
   lift $ foldM_ (runIteration trainData validateData) net0 [1 .. iterations]
- where
-  -- TODO train calls applyUpdate and backPropagate
-  -- Goal: Override those functions
-  trainEach rate' !network (i, o) = train rate' network i o
+  where
+    -- TODO train calls applyUpdate and backPropagate
+    -- Goal: Override those functions
+    trainEach rate' !network (i, o) = train rate' network i o
 
-  runIteration trainRows validateRows net i = do
-    let trained' = foldl' (trainEach (rate{learningRate = learningRate rate * 0.9 ^ i})) net trainRows
-    let res = fmap (\(rowP, rowL) -> (rowL,) $ runNet trained' rowP) validateRows
-    let res' = fmap (\(S1D label, S1D prediction) -> (maxIndex (SA.extract label), maxIndex (SA.extract prediction))) res
-    print trained'
-    putStrLn $ "Iteration " ++ show i ++ ": " ++ show (length (filter ((==) <$> fst <*> snd) res')) ++ " of " ++ show (length res')
-    return trained'
+    runIteration trainRows validateRows net i = do
+      let trained' = foldl' (trainEach (rate {learningRate = learningRate rate * 0.9 ^ i})) net trainRows
+      let res = fmap (\(rowP, rowL) -> (rowL,) $ runNet trained' rowP) validateRows
+      let res' = fmap (\(S1D label, S1D prediction) -> (maxIndex (SA.extract label), maxIndex (SA.extract prediction))) res
+      print trained'
+      putStrLn $ "Iteration " ++ show i ++ ": " ++ show (length (filter ((==) <$> fst <*> snd) res')) ++ " of " ++ show (length res')
+      return trained'
 
 -- main :: ExceptT String IO ()
 -- main = do
@@ -121,18 +126,18 @@ convTest iterations trainFile validateFile rate = do
 -- TODO "run" convTestDP  with net0 trainData
 
 convTestDP ::
-  forall e iterations s layers shapes.
+  forall e iterations s. -- TODO add this back ater layers shapes.
   (TL.KnownNat iterations) =>
-  [LabeledInput shapes] ->
-  Network layers shapes ->
+  [LabeledInput Shapes'] ->
+  Network Layers Shapes' ->
   LearningParameters ->
-  Solo.PM (Solo.ScalePriv (Solo.TruncatePriv e Solo.Zero s) iterations) (Network layers shapes) -- ExceptT String IO ()
+  Solo.PM (Solo.ScalePriv (Solo.TruncatePriv e Solo.Zero s) iterations) (Network Layers Shapes') -- ExceptT String IO ()
 convTestDP trainData initialNetwork rate = do
   Solo.seqloop @iterations (runIteration trainData) initialNetwork
- where
-  runIteration trainRows i net = do
-    let trained' = trainDP @e @s @layers @shapes (rate{learningRate = learningRate rate * 0.9 ^ i}) net trainRows
-    trained'
+  where
+    runIteration trainRows i net = do
+      let trained' = trainDP @e @s (rate {learningRate = learningRate rate * 0.9 ^ i}) net trainRows
+      trained'
 
 -- TODO do we need to override this?
 -- backPropagate :: SingI (Last shapes)
@@ -151,19 +156,27 @@ type LabeledInput shapes = (S (Head shapes), S (Last shapes))
 -- TODO override this to do DP things
 -- Update a network with new weights after training with an instance.
 trainDP ::
-  forall ε s layers shapes.
-  SingI (Last shapes) =>
+  forall ε s. -- TODO this was getting a bit too abstract. add this back later: layers shapes.
+  SingI (Last Shapes') =>
   LearningParameters ->
-  Network layers shapes ->
-  [LabeledInput shapes] -> -- Should we be taking in the training data as sensitive list here? or expect the caller to provide it?
-  Solo.PM (Solo.TruncatePriv ε Solo.Zero s) (Network layers shapes)
+  Network Layers Shapes' ->
+  [LabeledInput Shapes'] -> -- Should we be taking in the training data as sensitive list here? or expect the caller to provide it?
+  Solo.PM (Solo.TruncatePriv ε Solo.Zero s) (Network Layers Shapes')
 trainDP rate network trainRows = do
   -- newtype SList (m :: CMetric) (f :: SEnv -> *) (s :: SEnv) = SList_UNSAFE {unSList :: [f s]}
-  let sensitiveTrainRows = Solo.SList_UNSAFE @Solo.L2 @_ @s $ STRAINROW_UNSAFE @Solo.Disc @shapes <$> trainRows
+  let sensitiveTrainRows = Solo.SList_UNSAFE @Solo.L2 @_ @s $ STRAINROW_UNSAFE @Solo.Disc @Shapes' <$> trainRows
       gradSum = clippedGrad network sensitiveTrainRows
-      gradSumUnsafe = unSGrad gradSum -- Turns this to a number?
+      gradSumUnsafe = Solo.D_UNSAFE $ gradsToDouble $ unSGrad gradSum -- Turn this to a number?
   noisyGrad <- Solo.laplace @ε @s gradSumUnsafe -- Who knows what will go here????
-  return $ applyUpdate rate network (noisyGrad / (length trainRows))
+  return $ applyUpdate rate network (noisyGrad / (length trainRows)) -- TODO this expects Gradients not a single gradient
+  where
+    -- I am confused here.
+    -- Gradients is a list of Gradient
+    -- I do a destrucutre to get a single Gradient x
+    -- But Gradient is polymorphic on some x?
+    -- So how would I make it a Double?
+    gradsToDouble :: Gradients Layers -> Double
+    gradsToDouble (gradient :/> gradientRest) = undefined
 
 -- TODO reference this:
 -- https://hackage.haskell.org/package/grenade-0.1.0/docs/src/Grenade-Core-Network.html#Network
@@ -185,10 +198,10 @@ clippedGrad network trainRows =
       clippedGrads = map l2clip grads
       gradSum = columnSum clippedGrads -- sum the gradients, column-wise, to get 1-sensitive val
    in SGRAD_UNSAFE gradSum
- where
-  oneGrad (i, o) = backPropagate network i o
-  l2clip = undefined -- TODO
-  columnSum = undefined -- TODO
+  where
+    oneGrad (i, o) = backPropagate network i o
+    l2clip = undefined -- TODO
+    columnSum = undefined -- TODO
 
 data MnistOpts = MnistOpts FilePath FilePath Int LearningParameters
 
